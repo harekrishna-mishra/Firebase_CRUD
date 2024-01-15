5f65651d47750097f40f13dc93368a955dd8859c
harekrishna mishra <harekrishna.mishra@oodles.io>
2024-01-15 13:57:55 +0530

    implement lazy loading

--- src/App.js
+++ src/App.js
@@ -2,32 +2,40 @@ import React from "react";
 import "./App.css";
 import Navv from "./components/Navv";
 import Home from "./components/Home";
-import About from "./components/About";
+// import About from "./components/About";
 import PlaceOrder from "./components/PlaceOrder";
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import NoMathch from "./components/NoMathch";
 import Porduct from "./components/Porduct";
 import Electronics from "./components/Electronics";
 import Groery from "./components/Groery";
 import User from "./components/User";
 import UsersDetails from "./components/UsersDetails";
+const LazyAbout = React.lazy(() => import("./components/About"));
 
 function App() {
   return (
     <div className="">
-      <Navv/>
+      <Navv />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="order-summary" element={<PlaceOrder/>}></Route>
-        <Route path="*" element={<NoMathch/>}></Route>
-        <Route path="/product" element={<Porduct/>}>
-          <Route index element={<Electronics/>}></Route>
-          <Route path="electronics" element={<Electronics/>}></Route>
-          <Route path="grocery" element={<Groery/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/about"
+          element={
+            <React.Suspense fallback="Loading">
+              <LazyAbout />
+            </React.Suspense> 
+          }
+        ></Route>
+        <Route path="order-summary" element={<PlaceOrder />}></Route>
+        <Route path="*" element={<NoMathch />}></Route>
+        <Route path="/product" element={<Porduct />}>
+          <Route index element={<Electronics />}></Route>
+          <Route path="electronics" element={<Electronics />}></Route>
+          <Route path="grocery" element={<Groery />}></Route>
         </Route>
-        <Route path="/users" element={<User/>}>
-          <Route index path=":userId" element={<UsersDetails/>}></Route>
+        <Route path="/users" element={<User />}>
+          <Route index path=":userId" element={<UsersDetails />}></Route>
         </Route>
       </Routes>
     </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { useSearchParams } from 'react-router-dom'
 
 function User() {
+  const [serchParams, setSearchParams] = useSearchParams()
+  const showActiveUsers = serchParams.get("filter") === "active"
   return (
     <div className='container form_data my-5 p-5'>
+      <div className='d-flex justify-content-around'>
+        <Button  onClick={()=> setSearchParams({ filter: "active"})}>Active user</Button>
+        <Button  onClick={()=> setSearchParams({})}>Reset filter</Button>
+      </div>
       <h3>All Users</h3>
       <Outlet/>
+
+      {
+        showActiveUsers ? (<h3>Only Active Users</h3> ): (<h3>Show all users</h3>)
+      }
     </div>
   )
 }
